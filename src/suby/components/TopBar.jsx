ad9c785657d40d814aed8468f30da48447d2b6dc
kandyula akhil <akhilkandayala@gmail.com>
2025-07-29 23:45:38 +0530

    added styles

--- src/suby/components/TopBar.jsx
+++ src/suby/components/TopBar.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { CiLocationOn } from "react-icons/ci";
+import { MdOutlineKeyboardArrowDown } from "react-icons/md";
+import { BiSolidOffer } from "react-icons/bi";
+import { MdOutlineHelp } from "react-icons/md";
+import { BsFillPersonFill } from "react-icons/bs";
+import { FaShoppingCart } from "react-icons/fa";
 
 const TopBar = () => {
   return (
@@ -8,14 +14,31 @@ const TopBar = () => {
             <Link to='/' className='link'>
             <h2>Food Fantasy</h2>
             </Link>
+        
+        <div className="loc">
+          <CiLocationOn className="loc-icon" />
+          <p>maisammaguda,dulapally,kompally</p>
+          <MdOutlineKeyboardArrowDown className="loc-arrow"/>
         </div>
-        <div class="search-box">
-        <input type="text" placeholder="Search..." />
-        <button type="submit">Search</button>
         </div>
 
-        <div className="userAuth">
-            Login
+        <div className="navright">
+          <div className="navr">
+            <BiSolidOffer />
+            <p>Offers</p>
+          </div>
+          <div className="navr">
+            <MdOutlineHelp />
+            <p>Help</p>
+          </div>
+          <div className="navr">
+            <BsFillPersonFill />
+            <p>Sign In</p>
+          </div>
+          <div className="navr">
+            <FaShoppingCart />
+            <p>Cart</p>
+          </div>
         </div>
    </section>
   )
